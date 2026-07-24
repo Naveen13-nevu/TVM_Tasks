44e1471d6a422206976ba571363bd007eeee81ca
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-24 12:39:27 +0530

    i am practicing the concept about hoisting class and objects

---
+++ Day4/ExecutionContext.js
@@ -0,0 +1,33 @@
+
+// let name = "Naveen";
+
+// function all() {
+//     console.log(name);
+//     let y = 10;
+//     console.log("Arun");
+    
+// }
+// all();
+// console.log(y);
+
+
+console.log("Start");
+
+function all() {
+    console.log("Hi");
+    
+}
+all();
+
+function bye() {
+    console.log("Bye");
+    
+}
+bye();
+
+function gud() {
+    console.log("Gud night");
+    
+}
+gud();
+console.log("stop");
---
+++ Day4/Hoisting.js
@@ -0,0 +1,19 @@
+// let a;
+// console.log(a);
+// a =10;
+
+// console.log(a);
+// let a = 10;
+
+// console.log(a);
+// var a =10;
+
+greet();
+function greet() {
+    console.log("greet");
+}
+let con =()=> {
+    console.log("top");
+    
+}
+con();
