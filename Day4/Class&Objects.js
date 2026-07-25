8cfd8aed4705547f4a703f670aa099f462b3152c
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-25 20:04:20 +0530

    update

---
+++ Day4/Class&Objects.js
@@ -0,0 +1,42 @@
+// const student1 = {
+//     name:"Naveen",
+//     age:12
+// }
+
+// console.log(student1);
+
+// const student2 = {
+//     name:"Naveen",
+//     age:12
+// }
+
+// console.log(student2);
+
+// const student3= {
+//     name:"Naveen",
+//     age:12
+// }
+
+// console.log(student3);
+// const student4 = {
+//     name:"Naveen",
+//     age:12
+// }
+
+// console.log(student4);
+
+
+class student{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let Student1 = new student("Naveen",12);
+let Student2 = new student("Naveen",12);
+let Student3 = new student("Naveen",12);
+console.log(Student1);
+console.log(Student2);
+console.log(Student3);
+
---
+++ Day4/global.js
@@ -0,0 +1,24 @@
+
+// let a =10;
+// console.log(a);
+
+// function all() {
+    
+//     console.log("Naveen");
+    
+// }
+// all();
+
+// lol();
+// function lol() {
+    
+//     console.log("Kumar");
+    
+// }
+
+
+
+console.log(a);
+let a =10;
+
+
